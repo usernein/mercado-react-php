37fd2471bf567651361e8d921d2e80dd67ec3132
Cezar Pauxis <cezarhpx@gmail.com>
2023-03-06 01:30:04 -0300

    combobox back with its normal behavior

--- react-app/src/components/ProductCombobox/ProductCombobox.jsx
+++ react-app/src/components/ProductCombobox/ProductCombobox.jsx
@@ -1,5 +1,5 @@
 import { Combobox } from "@headlessui/react";
-import React, { useState } from "react";
+import React from "react";
 import {
     StyledComboboxInput,
     StyledComboboxOptionItem,
@@ -7,33 +7,24 @@ import {
 } from "./ProductCombobox.style";
 
 function ProductCombobox(props) {
-    const [inputFocused, setInputFocused] = useState(false);
     return (
         <Combobox value={props.defaultValue} onChange={props.onChange}>
-            {({ open }) => (
-                <>
-                    {(open || inputFocused) && (
-                        <Combobox.Options as={StyledComboboxOptionList} static>
-                            {props.products.map((product) => (
-                                <Combobox.Option
-                                    key={product.id}
-                                    value={product.name}
-                                    as={StyledComboboxOptionItem}
-                                >
-                                    {product.name}
-                                </Combobox.Option>
-                            ))}
-                        </Combobox.Options>
-                    )}
-                    <Combobox.Input
-                        onFocus={() => setInputFocused(true)}
-                        onBlur={() => setInputFocused(false)}
-                        onChange={props.onInputChange}
-                        as={StyledComboboxInput}
-                        placeholder="Produto"
-                    />
-                </>
-            )}
+            <Combobox.Options as={StyledComboboxOptionList}>
+                {props.products.map((product) => (
+                    <Combobox.Option
+                    key={product.id}
+                    value={product.name}
+                    as={StyledComboboxOptionItem}
+                    >
+                        {product.name}
+                    </Combobox.Option>
+                ))}
+            </Combobox.Options>
+            <Combobox.Input
+                onChange={props.onInputChange}
+                as={StyledComboboxInput}
+                placeholder="Produto"
+            />
         </Combobox>
     );
 }
